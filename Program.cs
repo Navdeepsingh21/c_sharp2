92ff6bf9e1660ebbff8010225770a7a3ab79b866
Admin <Admin@DESKTOP-POQE336>
2019-05-22 11:23:22 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("\nfirst number :");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("\nsecond number :");
+            b = int.Parse(Console.ReadLine());
+            c = a;
+            a = b;
+            b = c;
+            Console.Write("\nAfter Swapping :");
+            Console.Write("\nFirst Number :" + a);
+            Console.Write("\nSecond Number :" + b);
+            Console.Read();
+
+        }
+    }
+}
